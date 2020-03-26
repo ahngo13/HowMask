62aaa8a362c6add9e929f0c1688be32a2cccc0d5
Hamlet Shu <ahngo13@gmail.com>
2020-03-26 16:35:52 +0900

    index, login 수정

--- howmask/src/login.jsx
+++ howmask/src/login.jsx
@@ -1,39 +1,8 @@
 import React from "react";
-import { Navbar } from 'react-bootstrap';
-import { NavLink } from "react-router-dom";
 
 const Login = () => {
-    const navbarStyle = {
-        margin : "0 auto"
-    }
-    const navLinkStyle={
-        margin : 5
-    }
     return(
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/" style={navbarStyle}>
-            <img
-                alt=""
-                src="/logo.svg"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-            />{' '}
-            마스크 어때?
-            </Navbar.Brand>
-            <NavLink style={navLinkStyle} to="/map_p">map_p</NavLink>
-            <NavLink style={navLinkStyle} to="/store">store</NavLink>
-            <NavLink style={navLinkStyle} to="/register">register</NavLink>
-            <NavLink style={navLinkStyle} to="/modify">modify</NavLink>
-            <NavLink style={navLinkStyle} to="/checkPw">checkPw</NavLink>
-            <NavLink style={navLinkStyle} to="/birth">birth</NavLink>
-            <NavLink style={navLinkStyle} to="/location">location</NavLink>
-            <NavLink style={navLinkStyle} to="/map">map</NavLink>
-            <NavLink style={navLinkStyle} to="/marker">marker</NavLink>
-            <NavLink style={navLinkStyle} to="/search">search</NavLink>
-            <NavLink style={navLinkStyle} to="/storeInfoUpdate">storeInfoUpdate</NavLink>
-            <NavLink style={navLinkStyle} to="/storeInfo">storeInfo</NavLink>
-        </Navbar>
+        <>로그인</>
     )
 }
 
