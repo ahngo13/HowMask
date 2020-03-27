e8c5f92c81b0842d887373e5ad844151d19fcc31
pickle-radish <tkdansg0222@gmail.com>
2020-03-27 14:17:59 +0900

    add search bar in map

--- howmask/src/map_p.jsx
+++ howmask/src/map_p.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Map from './map';
+import Search from './search';
 
 const MapP = () => {
     return(
-        <div>
+        <div id="mapPage">
+            <div id='searchDiv'>
+                <Search />
+            </div>
             <Map />
         </div>
         
