0ec25b795b5d985e1db0440ac0c7cbcbc8bacf36
pickle-radish <tkdansg0222@gmail.com>
2020-03-27 14:40:06 +0900

    add search bar in the main page

--- howmask/src/index.jsx
+++ howmask/src/index.jsx
@@ -4,7 +4,9 @@ import { HashRouter, NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar } from "react-bootstrap";
 import Router from "./router";
+import Search from './search';
 
+import './css/main.css';
 import './css/map.css';
 
 const navbarStyle = {
@@ -15,50 +17,58 @@ const navLinkStyle = {
 };
 
 ReactDOM.render(
-  <HashRouter>
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="/" style={navbarStyle}>
-        <img alt="" src="/logo.svg" width="30" height="30" className="d-inline-block align-top" />{" "}
-        마스크 어때?
-      </Navbar.Brand>
-      <NavLink style={navLinkStyle} to="/map_p">
-        map_p
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/store">
-        store
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/register">
-        register
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/modify">
-        modify
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/checkPw">
-        checkPw
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/birth">
-        birth
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/location">
-        location
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/map">
-        map
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/marker">
-        marker
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/search">
-        search
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/storeInfoUpdate">
-        storeInfoUpdate
-      </NavLink>
-      <NavLink style={navLinkStyle} to="/storeInfo">
-        storeInfo
-      </NavLink>
-    </Navbar>
-    <Router />
-  </HashRouter>,
+  <div>
+    <HashRouter>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="/" style={navbarStyle}>
+          <img alt="" src="/logo.svg" width="30" height="30" className="d-inline-block align-top" />{" "}
+          마스크 어때?
+        </Navbar.Brand>
+        <NavLink style={navLinkStyle} to="/map_p">
+          map_p
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/store">
+          store
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/register">
+          register
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/modify">
+          modify
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/checkPw">
+          checkPw
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/birth">
+          birth
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/location">
+          location
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/map">
+          map
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/marker">
+          marker
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/search">
+          search
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/storeInfoUpdate">
+          storeInfoUpdate
+        </NavLink>
+        <NavLink style={navLinkStyle} to="/storeInfo">
+          storeInfo
+        </NavLink>
+      </Navbar>
+      <Router />
+    </HashRouter>
+    <div id="buyMask">
+      오늘 마스크 구입 끝자리 0, 5
+    </div>
+    <div id="mainSearch">
+      <Search />
+    </div>
+  </div>,
   document.getElementById("container")
 );
