493e233a9c18b64ff3ec55835bc05b7dcc0df373
pickle-radish <tkdansg0222@gmail.com>
2020-03-28 19:56:38 +0900

    update get api by location

--- howmask_server/routes/maskRouter.js
+++ howmask_server/routes/maskRouter.js
@@ -12,8 +12,7 @@ router.post("/storesByGeo", async (req, res) => {
   console.log(req.body.lng);
   axios
     .get(
-      "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json",
-      send_param
+      `https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=${req.body.lat}&lng=${req.body.lng}&m${req.body.m}`
     )
     //정상 수행
     .then(returnData => {
