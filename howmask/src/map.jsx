6108936351142fc79fdd4fb7b49d9c2871b04e47
jinah <jinah0818@gmail.com>
2020-03-30 22:58:10 +0900

    Update Stock style by Mask API

--- howmask/src/map.jsx
+++ howmask/src/map.jsx
@@ -1,186 +1,171 @@
-import React, { useState, useEffect } from 'react';
-import {usePosition} from 'use-position';
-import axios from 'axios';
-import StoreModal from './store_info';
+import React, { useState, useEffect } from "react";
+import { usePosition } from "use-position";
+import axios from "axios";
+import StoreModal from "./store_info";
 const { kakao } = window;
 const { daum } = window;
 
 axios.defaults.withCredentials = true;
-const headers={withCredentials:true};
+const headers = { withCredentials: true };
 
-const Map = (props) => {
+const Map = props => {
+  const { latitude, longitude } = usePosition();
+  const [positions, setPositions] = useState();
+  const [coords, setCoords] = useState({
+    lat: null,
+    lng: null
+  });
+  const [level, setLevel] = useState(3);
+  const [modalShow, setModalShow] = useState(false);
+  const [storeInfo, setStoreInfo] = useState();
 
-    const {latitude, longitude} = usePosition();
-    const [positions, setPositions] = useState();
-    const [coords, setCoords] = useState({
-        lat: null,
-        lng: null,
-    });
-    const [level, setLevel] = useState(3);
-    const [modalShow, setModalShow] = useState(false);
-    const [storeInfo, setStoreInfo] = useState();
+  console.log("latitude:" + latitude);
+  console.log("longitude:" + longitude);
 
-
-    
-    console.log("latitude:" +latitude);
-    console.log("longitude:" +longitude);
-
-    async function getInfoByGeo (lat, lng) {
-        const info = []
-        const send_param={
-            headers,
-            lat,
-            lng,
-            m:5000,
-        }
-        const result = await axios.post('http://localhost:8080/mask/storesByGeo', send_param);
-        if(result.data.storeList){
-            result.data.storeList.forEach((item)=>{  
-                info.push({
-                    title: item.name, 
-                    latlng: new kakao.maps.LatLng(item.lat, item.lng),
-                    storeInfo: {
-                        addr: item.addr,
-                        name: item.name,
-                        stock: item.remain_stat,
-                    }
-                })
-            })
-        }
-        console.log(result.data.storeList);
-        setPositions(info);
-        
+  async function getInfoByGeo(lat, lng) {
+    const info = [];
+    const send_param = {
+      headers,
+      lat,
+      lng,
+      m: 5000
+    };
+    const result = await axios.post("http://localhost:8080/mask/storesByGeo", send_param);
+    if (result.data.storeList) {
+      result.data.storeList.forEach(item => {
+        info.push({
+          title: item.name,
+          latlng: new kakao.maps.LatLng(item.lat, item.lng),
+          storeInfo: {
+            addr: item.addr,
+            name: item.name,
+            stock: item.remain_stat
+          }
+        });
+      });
     }
-    async function getInfoByAddr () {
-        const info = []
-        const send_param={
-            headers,
-            address:"고양시 덕양구 신원동",
-        }
-        const result = await axios.post('http://localhost:8080/mask/storesByAddr', send_param);
-        if(result.data.storeList){
-            result.data.storeList.forEach((item)=>{  
-                info.push({
-                    title: item.name, 
-                    latlng: new kakao.maps.LatLng(item.lat, item.lng),
-                })
-            })
-        }
-
-        setPositions(info);
-        
+    console.log(result.data.storeList);
+    setPositions(info);
+  }
+  async function getInfoByAddr() {
+    const info = [];
+    const send_param = {
+      headers,
+      address: "고양시 덕양구 신원동"
+    };
+    const result = await axios.post("http://localhost:8080/mask/storesByAddr", send_param);
+    if (result.data.storeList) {
+      result.data.storeList.forEach(item => {
+        info.push({
+          title: item.name,
+          latlng: new kakao.maps.LatLng(item.lat, item.lng)
+        });
+      });
     }
 
-    useEffect(() => {       
-        if(latitude && longitude && !(coords.lat && coords.lng)){
-            getInfoByGeo(latitude, longitude);
+    setPositions(info);
+  }
+
+  useEffect(() => {
+    if (latitude && longitude && !(coords.lat && coords.lng)) {
+      getInfoByGeo(latitude, longitude);
+    }
+  }, [latitude, longitude]);
+
+  useEffect(() => {
+    if (coords.lat && coords.lng) {
+      getInfoByGeo(coords.lat, coords.lng);
+      console.log(positions);
+    }
+  }, [coords]);
+
+  useEffect(() => {
+    console.log(props.keyWord);
+
+    if (latitude && longitude) {
+      // getStoreInfo();
+
+      // 지도의 중심 위치 지정
+      let lat, lng;
+      if (coords.lat === null) {
+        lat = latitude;
+        lng = longitude;
+      } else {
+        lat = coords.lat;
+        lng = coords.lng;
+      }
+
+      let el = document.getElementById("map");
+      let map = new daum.maps.Map(el, {
+        center: new daum.maps.LatLng(lat, lng),
+        level
+      });
+
+      // 현재 위치에 표시될 마커의 위치입니다
+      var markerPosition = new kakao.maps.LatLng(latitude, longitude);
+
+      var imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+      console.log(positions);
+
+      if (positions) {
+        let selectedMarker = null;
+        for (var i = 0; i < positions.length; i++) {
+          // 마커 이미지의 이미지 크기 입니다
+          var imageSize = new kakao.maps.Size(24, 35);
+
+          // 마커 이미지를 생성합니다
+          var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+          // 마커를 생성합니다
+          var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image: markerImage // 마커 이미지
+          });
+
+          // 마커에 클릭이벤트를 등록합니다
+          //kakao.maps.event.addListener(marker, 'click', makeClickListener(map, marker, infowindow));
+
+          (function(marker, info) {
+            // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시합니다
+            kakao.maps.event.addListener(marker, "click", function() {
+              setModalShow(true);
+              setStoreInfo(info);
+            });
+          })(marker, positions[i].storeInfo);
         }
-    },[latitude, longitude]);
+      }
 
-    useEffect(() => {       
-        if(coords.lat && coords.lng){
-            getInfoByGeo(coords.lat, coords.lng);
-            console.log(positions);
-        }
-    },[coords]);
-    
-    useEffect(() =>{
-        console.log(props.keyWord);
+      //현재 위치 마커를 생성합니다
+      marker = new kakao.maps.Marker({
+        map: map,
+        position: markerPosition
+      });
 
-        if(latitude && longitude){
-            // getStoreInfo();
-            
-            // 지도의 중심 위치 지정
-            let lat, lng;
-            if(coords.lat === null){
-                lat = latitude
-                lng = longitude
-            }else{
-                lat = coords.lat
-                lng = coords.lng
-            }
+      // 마우스 드래그로 지도 이동이 완료되었을 때 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+      kakao.maps.event.addListener(map, "dragend", function() {
+        // 지도 중심좌표를 얻어옵니다
+        let latlng = map.getCenter();
 
-            let el = document.getElementById('map');
-            let map = new daum.maps.Map(el, {
-                center: new daum.maps.LatLng(lat, lng),
-                level,
-            });
+        setCoords({ lat: latlng.getLat(), lng: latlng.getLng() });
+      });
+      // 지도가 확대 또는 축소되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+      kakao.maps.event.addListener(map, "zoom_changed", function() {
+        // 지도의 현재 레벨을 얻어옵니다
+        let level = map.getLevel();
 
-            // 현재 위치에 표시될 마커의 위치입니다 
-            var markerPosition  = new kakao.maps.LatLng(latitude, longitude); 
+        setLevel(level);
+      });
+    }
+  }, [positions]);
 
-            var imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-
-            console.log(positions);
-
-            if(positions){
-                let selectedMarker = null;
-                for (var i = 0; i < positions.length; i ++) {
-                    // 마커 이미지의 이미지 크기 입니다
-                    var imageSize = new kakao.maps.Size(24, 35); 
-                    
-                    // 마커 이미지를 생성합니다    
-                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-                    // 마커를 생성합니다
-                    var marker = new kakao.maps.Marker({
-                        map: map, // 마커를 표시할 지도
-                        position: positions[i].latlng, // 마커를 표시할 위치
-                        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다 
-                        image : markerImage, // 마커 이미지 
-                        
-                    });
-
-                    
-                    
-                    // 마커에 클릭이벤트를 등록합니다
-                    //kakao.maps.event.addListener(marker, 'click', makeClickListener(map, marker, infowindow));
-                   
-                    (function(marker, info) {
-                        // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시합니다 
-                        kakao.maps.event.addListener(marker, 'click', function() {
-                           setModalShow(true);
-                           setStoreInfo(info);
-                        });                       
-                    })(marker, positions[i].storeInfo);
-                }
-
-
-            }
-
-            //현재 위치 마커를 생성합니다
-            marker = new kakao.maps.Marker({
-                map:map,
-                position: markerPosition
-            });
-
-            // 마우스 드래그로 지도 이동이 완료되었을 때 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
-            kakao.maps.event.addListener(map, 'dragend', function() {        
-                
-                // 지도 중심좌표를 얻어옵니다 
-                let latlng = map.getCenter(); 
-
-                setCoords({lat: latlng.getLat(), lng: latlng.getLng()});
-                
-            });
-            // 지도가 확대 또는 축소되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
-            kakao.maps.event.addListener(map, 'zoom_changed', function() {        
-                
-                // 지도의 현재 레벨을 얻어옵니다
-                let level = map.getLevel();
-                
-                setLevel(level);
-            });
-            
-        }
-        
-    },[positions]);
-
-    return(
-        <div>
-            <div className="App" id="map"></div>
-            <StoreModal show={modalShow} storeInfo={storeInfo} onHide={() => setModalShow(false)} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="App" id="map"></div>
+      <StoreModal show={modalShow} storeInfo={storeInfo} onHide={() => setModalShow(false)} />
+    </div>
+  );
+};
 
 export default Map;
--- howmask/src/store_info.jsx
+++ howmask/src/store_info.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button, Badge, Table, Card, Container, Col, Row } from "react-bootstrap";
 import Comment from "./comment";
 
@@ -20,11 +20,48 @@ function RegisterStoreAccount() {
 
 //판매처 상세정보 Modal
 function StoreInfoModal(props) {
+  const [stockColor, setStockColor] = useState();
+  const [stockText, setStockText] = useState();
+  function howMany() {
+    const stock = props.storeInfo.stock;
+    return (
+      <div>
+        {(() => {
+          if (stock === "plenty") {
+            setStockColor("success");
+            setStockText("100개 이상");
+            return;
+          } else if (stock === "some") {
+            setStockColor("primary");
+            setStockText("30개 이상~100개 미만");
+            return;
+          } else if (stock === "few") {
+            setStockColor("warning");
+            setStockText("2개 이상~30개 미만");
+            return;
+          } else if (stock === "empty") {
+            setStockColor("danger");
+            setStockText("1개 이하");
+            return;
+          } else {
+            setStockColor("secondary");
+            setStockText("재고 없음");
+            return;
+          }
+        })()}
+      </div>
+    );
+  }
+  useEffect(() => {
+    howMany();
+  });
+
   return (
     <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          {storeInfo.storeName} <Badge variant="success">{storeInfo.howMany}</Badge>
+          {props.storeInfo.name}
+          &nbsp;&nbsp;&nbsp;<Badge variant={stockColor}>{stockText}</Badge>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
@@ -35,13 +72,13 @@ function StoreInfoModal(props) {
                 <strong>
                   <font color="#1a0066">판매처 종류</font>
                 </strong>
-                &nbsp;&nbsp;&nbsp; {storeInfo.storeType}
+                &nbsp;&nbsp;&nbsp; 약국/하나로마트/우체국/기타
               </td>
               <td>
                 <strong>
                   <font color="#1a0066">주소</font>
                 </strong>
-                &nbsp;&nbsp;&nbsp; {props.storeInfo.name}
+                &nbsp;&nbsp;&nbsp; {props.storeInfo.addr}
               </td>
             </tr>
             <tr>
@@ -49,14 +86,13 @@ function StoreInfoModal(props) {
                 <strong>
                   <font color="#1a0066">영업시간</font>
                 </strong>
-                &nbsp;&nbsp;&nbsp; ({storeInfo.operatingDate}) {storeInfo.operatingTimeStart}~
-                {storeInfo.operatingTimeEnd}
+                &nbsp;&nbsp;&nbsp; (요일) 시작시간~종료시간
               </td>
               <td>
                 <strong>
                   <font color="#1a0066">판매예정시간</font>
                 </strong>
-                &nbsp;&nbsp;&nbsp; {storeInfo.soldExpectedTime}
+                &nbsp;&nbsp;&nbsp; 실제 마스크 판매시간
               </td>
             </tr>
             <tr>
@@ -64,13 +100,13 @@ function StoreInfoModal(props) {
                 <strong>
                   <font color="#1a0066">평균 재고수량</font>
                 </strong>
-                &nbsp;&nbsp;&nbsp; {storeInfo.stockAverage}개
+                &nbsp;&nbsp;&nbsp; 000개
               </td>
               <td>
                 <strong>
                   <font color="#1a0066">유아용 마스크 판매여부</font>
                 </strong>
-                &nbsp;&nbsp;&nbsp; <Badge variant="primary">{storeInfo.kidsMaskYN}</Badge>
+                &nbsp;&nbsp;&nbsp; <Badge variant="primary">유(무)</Badge>
               </td>
             </tr>
           </tbody>
@@ -113,20 +149,13 @@ function StoreInfoModal(props) {
   );
 }
 
-
-
 function StoreInfo(props) {
-  
-  let modal
-  if(props.storeInfo){
-    modal = <StoreInfoModal show={props.show} storeInfo={props.storeInfo} onHide={props.onHide} />
+  let modal;
+  if (props.storeInfo) {
+    modal = <StoreInfoModal show={props.show} storeInfo={props.storeInfo} onHide={props.onHide} />;
   }
 
-  return (
-    <>
-      {modal}
-    </>
-  );
+  return <>{modal}</>;
 }
 
 export default StoreInfo;
