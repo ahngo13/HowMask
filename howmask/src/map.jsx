b80ca47842a3b6812a5bf119ec62cda213afe705
pickle-radish <tkdansg0222@gmail.com>
2020-03-30 22:09:36 +0900

    update search function

--- howmask/src/map.jsx
+++ howmask/src/map.jsx
@@ -19,6 +19,7 @@ const Map = (props) => {
     const [level, setLevel] = useState(3);
     const [modalShow, setModalShow] = useState(false);
     const [storeInfo, setStoreInfo] = useState();
+    const [word, setWord] = useState(props.keyWord);
 
 
     
@@ -51,11 +52,11 @@ const Map = (props) => {
         setPositions(info);
         
     }
-    async function getInfoByAddr () {
+    async function getInfoByAddr (keyWord) {
         const info = []
         const send_param={
             headers,
-            address:"고양시 덕양구 신원동",
+            address: keyWord,
         }
         const result = await axios.post('http://localhost:8080/mask/storesByAddr', send_param);
         if(result.data.storeList){
@@ -63,29 +64,46 @@ const Map = (props) => {
                 info.push({
                     title: item.name, 
                     latlng: new kakao.maps.LatLng(item.lat, item.lng),
-                })
-            })
+                    storeInfo: {
+                        addr: item.addr,
+                        name: item.name,
+                        stock: item.remain_stat,
+                    }
+                });
+            });
         }
 
         setPositions(info);
         
     }
 
-    useEffect(() => {       
-        if(latitude && longitude && !(coords.lat && coords.lng)){
-            getInfoByGeo(latitude, longitude);
+    useEffect(()=>{
+        setWord(props.keyWord);
+    }, [props.keyWord]);
+
+    useEffect(()=>{
+        if(word){
+            getInfoByAddr(word);
+        }
+    }, [word]);
+
+    useEffect(() => {    
+        if(!word){
+            if(latitude && longitude && !(coords.lat && coords.lng)){
+                getInfoByGeo(latitude, longitude);
+            }
         }
     },[latitude, longitude]);
 
-    useEffect(() => {       
-        if(coords.lat && coords.lng){
-            getInfoByGeo(coords.lat, coords.lng);
-            console.log(positions);
-        }
+    useEffect(() => {     
+        if(!word){
+            if(coords.lat && coords.lng){
+                getInfoByGeo(coords.lat, coords.lng);
+            }
+        }  
     },[coords]);
     
     useEffect(() =>{
-        console.log(props.keyWord);
 
         if(latitude && longitude){
             // getStoreInfo();
@@ -170,7 +188,36 @@ const Map = (props) => {
                 
                 setLevel(level);
             });
-            
+                        
+            // 주소-좌표 변환 객체를 생성합니다
+            var geocoder = new kakao.maps.services.Geocoder();
+
+            // 주소로 좌표를 검색합니다
+            if(word){
+                geocoder.addressSearch(word, function(result, status) {
+                    
+                    // 정상적으로 검색이 완료됐으면 
+                    if (status === kakao.maps.services.Status.OK) {
+                        
+                        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+                        
+                        // 결과값으로 받은 위치를 마커로 표시합니다
+                        var marker = new kakao.maps.Marker({
+                            map: map,
+                            position: coords
+                        });
+                    
+                        // 인포윈도우로 장소에 대한 설명을 표시합니다
+                        var infowindow = new kakao.maps.InfoWindow({
+                            content: `<div style="width:150px;text-align:center;padding:6px 0;">${word}</div>`
+                        });
+                        infowindow.open(map, marker);
+                        
+                        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                        map.setCenter(coords);
+                    } 
+                });    
+            }
         }
         
     },[positions]);
--- howmask_server/routes/maskRouter.js
+++ howmask_server/routes/maskRouter.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const axios = require("axios");
 
 router.post("/storesByGeo", async (req, res) => {
-  const send_param = {
-    lat: req.body.lat,
-    lng: req.body.lng,
-    m: req.body.m
-  };
+  
   axios
     .get(
       `https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=${req.body.lat}&lng=${req.body.lng}&m${req.body.m}`
@@ -24,12 +20,9 @@ router.post("/storesByGeo", async (req, res) => {
 });
 
 router.post("/storesByAddr", async (req, res) => {
-  const send_param = {
-    address: req.body.address
-  };
-
+  addr = encodeURI(req.body.address);
   await axios
-    .get(`https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json`, send_param)
+    .get(`https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address=${addr}`)
     //정상 수행
     .then(returnData => {
       console.log(returnData.data.stores);
