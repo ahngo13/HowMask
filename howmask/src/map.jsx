f4f88177420d8c2cc7bc931603f9b4196e414ee9
pickle-radish <tkdansg0222@gmail.com>
2020-03-30 13:53:45 +0900

    update move map function

--- howmask/src/map.jsx
+++ howmask/src/map.jsx
@@ -9,19 +9,24 @@ const headers={withCredentials:true};
 
 const Map = () => {
 
-    const {latitude, longitude, error} = usePosition();
+    const {latitude, longitude} = usePosition();
     const [positions, setPositions] = useState();
+    const [coords, setCoords] = useState({
+        lat: null,
+        lng: null,
+    });
+
 
     
     console.log("latitude:" +latitude);
     console.log("longitude:" +longitude);
 
-    async function getInfoByGeo () {
+    async function getInfoByGeo (lat, lng) {
         const info = []
         const send_param={
             headers,
-            lat:latitude,
-            lng:longitude,
+            lat,
+            lng,
             m:5000,
         }
         const result = await axios.post('http://localhost:8080/mask/storesByGeo', send_param);
@@ -59,39 +64,48 @@ const Map = () => {
 
     useEffect(() => {       
         if(latitude && longitude){
-            getInfoByGeo();
+            getInfoByGeo(latitude, longitude);
             console.log(positions);
         }
     },[latitude, longitude])
+
+    useEffect(() => {       
+        if(coords.lat && coords.lng){
+            getInfoByGeo(coords.lat, coords.lng);
+            console.log(positions);
+        }
+    },[coords])
+    
+
+
     
     useEffect(() =>{
 
         if(latitude && longitude){
             // getStoreInfo();
-        
-            
+            let lat, lng;
+            if(coords.lat === null){
+                lat = latitude
+                lng = longitude
+            }else{
+                lat = coords.lat
+                lng = coords.lng
+            }
+            console.log(lat);
+            console.log(lng);
             let el = document.getElementById('map');
             let map = new daum.maps.Map(el, {
-            center: new daum.maps.LatLng(latitude, longitude)
+            center: new daum.maps.LatLng(lat, lng)
             });
 
-            // 마커가 표시될 위치입니다 
+            // 현재 위치에 표시될 마커의 위치입니다 
             var markerPosition  = new kakao.maps.LatLng(latitude, longitude); 
-            /* const positions = [] 
-            list.forEach((item)=>{  
-                positions.push({
-                    title: item.name, 
-                    latlng: new kakao.maps.LatLng(item.lat, item.lng),
-                })
-            }) */
+        
 
             var imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
             console.log(positions);
-            // positions=secondPosition();
-            // console.log(positions[0].latlng);
 
-            // positions=position;
             if(positions){
                 for (var i = 0; i < positions.length; i ++) {
                     // 마커 이미지의 이미지 크기 입니다
@@ -113,6 +127,21 @@ const Map = () => {
             marker = new kakao.maps.Marker({
                 map:map,
                 position: markerPosition
+            });
+
+            // 마우스 드래그로 지도 이동이 완료되었을 때 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+            kakao.maps.event.addListener(map, 'dragend', function() {        
+                
+                // 지도 중심좌표를 얻어옵니다 
+                var latlng = map.getCenter(); 
+
+                setCoords({lat: latlng.getLat(), lng: latlng.getLng()});
+                console.log(coords);
+                var message = '변경된 지도 중심좌표는 ' + coords.lat + ' 이고, ';
+                message += '경도는 ' + coords.lng + ' 입니다';
+                
+                console.log(message);
+                
             });
         }
         
