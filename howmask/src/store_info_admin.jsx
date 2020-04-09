33e77ec1308e9a384c3cdc883e92d3af54bc6bc3
HamletShu <ahngo13@naver.com>
2020-04-09 13:49:49 +0900

    관리자 페이지 불필요한 소스 수정 및 디자인 수정

--- howmask/src/store_info_admin.jsx
+++ howmask/src/store_info_admin.jsx
@@ -1,66 +1,20 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { Form, Col } from "react-bootstrap";
-import axios from "axios";
-
-axios.defaults.withCredentials = true;
-const url = "localhost";
-const headers = { withCredentials: true };
 
 // 판매처 정보 관리 Form
 const StoreInfoAdmin = (props) => {
-  const [title, setTitle] = useState(" 판매처 정보 조회");
 
-  
   let storeForm;
 
-  const registerTitle = {
-    
-    margin: "auto",
-    textAlign: "center",
-  };
   const registerForm = {
     width: "100%",
     margin: "auto",
   };
   
-  async function getStoreDetail(props) {
-    console.log("getInfo");
-    console.log(props);
-   /*  const sendParam = { code, headers };
-
-    const result = await axios
-      .post(`http://${url}:8080/store/getStoreInfo`, sendParam)
-      .then((returnData) => {
-        console.log(returnData.data.info);
-        setStoreInfo(returnData.data.info);
-      })
-      .catch((err) => {
-        console.log(err);
-      }); */
-
-    console.log(props);
-
-
-    /* if (result.data.info) {
-      const info = result.data.info;
-      setCode(info.code);
-      setStoreNameState(info.storeName);
-      setBizCodeState(info.bizCode);
-      setAddrState(info.address);
-      setSellerNameState(info.sellerName);
-      setPhoneState(info.phone);
-      setEmailState(result.data.email);
-    } else {
-      console.log("setting fail");
-    } */
-  }
   if(props.storeInfo){
     storeForm = (
       <>
-        <h2 style={registerTitle}>{title}</h2>
         <Form style={registerForm}>
-          <Form.Text className="text-muted"></Form.Text>
-          <Form.Label>판매처 정보</Form.Label>
           <Form.Text className="text-muted"></Form.Text>
           <Form.Row>
             <Form.Group as={Col}>
